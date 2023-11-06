2d5a09e29fc0970d880cfbfdf08899d48acb80d3
Abdulla Al Helal <alhelal7778@gmail.com>
2023-11-06 11:31:07 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$(function(){
+    $('.').owlCarousel({
+        loop:true,
+        margin:30,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        smartSpeed: 500,
+        dots:false,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+                
+            },
+            1000:{
+                items:3,
+                
+            }
+        },
+        1200:{
+            items:4,
+            
+        }
+    }
+});
